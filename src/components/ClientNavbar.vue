8a770218c68838e5a515a051bbfdab3dfd978617
William <willam3305@gmail.com>
2022-08-25 22:00:19 +0800

    更新index響應式

--- src/components/ClientNavbar.vue
+++ src/components/ClientNavbar.vue
@@ -1,11 +1,6 @@
 <template>
   <nav
-    class="
-      navbar navbar-expand-lg navbar-light
-      bg-opacity-0
-      fixed-top
-      px-4
-    "
+    class="navbar navbar-expand-lg navbar-light bg-opacity-0 fixed-top px-4"
     :class="{ navShow: navScroll }"
   >
     <div class="container-fluid">
@@ -13,41 +8,99 @@
         <img class="nav-logo" src="../assets/images/logo.svg" alt="logo" />
       </router-link>
       <button
-        class="navbar-toggler"
+        class="btn d-lg-none d-block"
         type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarText"
-        aria-controls="navbarText"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasRight"
+        aria-controls="offcanvasRight"
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
-        <span class="bi bi-list text-light"></span>
+        <span class="bi bi-list fs-2 text-light"></span>
       </button>
+      <div
+        class="offcanvas offcanvas-end w-100 d-lg-none d-block navShow"
+        tabindex="-1"
+        id="offcanvasRight"
+        aria-labelledby="offcanvasRightLabel"
+      >
+        <div class="offcanvas-header pb-0">
+          <button
+            type="button"
+            class="btn bi bi-x-lg fs-4 text-light"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="offcanvas-body pt-0">
+          <ul class="navbar-nav text-center me-auto mb-2 mb-lg-0">
+            <li class="nav-item py-2 fs-4">
+              <router-link class="canvas-link text-light" to="index"
+                >返回首頁</router-link
+              >
+            </li>
+            <li class="nav-item py-2 fs-4">
+              <router-link class="canvas-link text-light" to="index">專輯列表</router-link>
+            </li>
+            <li class="nav-item py-2 fs-4">
+              <router-link class="canvas-link text-light" to="index"
+                >最愛商品</router-link
+              >
+            </li>
+            <li class="nav-item py-2 fs-4">
+              <router-link class="canvas-link text-light" to="index"
+                >購物車</router-link
+              >
+            </li>
+            <li class="nav-item py-2 fs-4">
+              <router-link class="canvas-link text-light" to="index"
+                >後台登入</router-link
+              >
+            </li>
+          </ul>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-icon text-light fs-4 d-lg-block d-none" href="#"
+                ><i class="bi bi-star"></i
+              ></a>
+            </li>
+            <li class="nav-item my-md-0 my-2">
+              <a class="nav-icon text-light fs-4 d-lg-block d-none" href="#"
+                ><i class="bi bi-cart2"></i
+              ></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-icon text-light fs-4 d-lg-block d-none" href="#"
+                ><i class="bi bi-person-workspace"></i
+              ></a>
+            </li>
+          </ul>
+        </div>
+      </div>
       <div class="collapse navbar-collapse nav-shift" id="navbarText">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-lg-flex d-none">
           <li class="nav-item">
-            <router-link class="nav-link text-light" to="/index">首頁</router-link>
+            <router-link class="nav-link text-light" to="/index"
+              >首頁</router-link
+            >
           </li>
           <li class="nav-item">
-            <a class="nav-link text-light" href="#">專輯</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-light" href="#">問與答</a>
+            <a class="nav-link text-light" href="#">專輯列表</a>
           </li>
         </ul>
         <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-icon text-light fs-4" href="#"
+            <a class="nav-icon text-light fs-4 d-lg-block d-none" href="#"
               ><i class="bi bi-star"></i
             ></a>
           </li>
           <li class="nav-item my-md-0 my-2">
-            <a class="nav-icon text-light fs-4" href="#"
+            <a class="nav-icon text-light fs-4 d-lg-block d-none" href="#"
               ><i class="bi bi-cart2"></i
             ></a>
           </li>
           <li class="nav-item">
-            <a class="nav-icon text-light fs-4" href="#"
+            <a class="nav-icon text-light fs-4 d-lg-block d-none" href="#"
               ><i class="bi bi-person-workspace"></i
             ></a>
           </li>
