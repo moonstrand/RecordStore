c49af2674217f8a15a3d4883ba94f79eff18c9a9
William <willam3305@gmail.com>
2022-09-12 14:33:47 +0800

    新增類別切換&關鍵字篩選功能

--- src/views/ClientProducts.vue
+++ src/views/ClientProducts.vue
@@ -20,19 +20,49 @@
           "
         >
           <li>
-            <a class="category-link">所有類別</a>
+            <a
+              class="category-link"
+              href="#"
+              :class="{ categoryActive: filterMode == false }"
+              @click.prevent="filterProducts()"
+              >所有類別</a
+            >
           </li>
           <li>
-            <a class="category-link" href="#">華語</a>
+            <a
+              class="category-link"
+              href="#"
+              :class="{ categoryActive: category == '西洋' }"
+              @click.prevent="filterProducts('西洋')"
+              >西洋</a
+            >
           </li>
           <li>
-            <a class="category-link" href="#">東洋</a>
+            <a
+              class="category-link"
+              href="#"
+              :class="{ categoryActive: category == '東洋' }"
+              @click.prevent="filterProducts('東洋')"
+              >東洋</a
+            >
           </li>
           <li>
-            <a class="category-link" href="#">西洋</a>
+            <a
+              class="category-link"
+              href="#"
+              :class="{ categoryActive: category == '華語' }"
+              @click.prevent="filterProducts('華語')"
+              >華語</a
+            >
           </li>
-          <li class="border-0">
-            <a class="category-link" href="#">純音樂</a>
+          <li>
+            <a
+              class="category-link"
+              href="#"
+              :class="{ categoryActive: category == '純音樂' }"
+              @click.prevent="filterProducts('純音樂')"
+              >純音樂</a
+            >
           </li>
         </ul>
         <div class="col-lg-4 d-flex justify-content-end mt-lg-0 mt-3">
@@ -41,6 +71,7 @@
               class="btn btn-outline-secondary rounded-0"
               type="button"
               id="button-addon1"
+              @click="keywordFilter"
             >
               <i class="bi bi-search"></i>
             </button>
@@ -50,6 +81,7 @@
               placeholder="請輸入關鍵字"
               aria-label="Example text with button addon"
               aria-describedby="button-addon1"
+              v-model="keyword"
             />
           </div>
         </div>
@@ -57,7 +89,7 @@
     </section>
     <!-- Products list -->
     <section class="products container pt-xl-3 pt-0 pb-xl-5 pb-0">
-      <div class="row">
+      <div class="row" v-if="!filterMode">
         <div
           class="col-xxl-3 col-lg-4 col-sm-6 mb-xxl-0"
           v-for="item in products"
@@ -93,18 +125,58 @@
           </div>
         </div>
       </div>
+      <div class="row" v-else>
+        <div
+          class="col-xxl-3 col-lg-4 col-sm-6 mb-xxl-0"
+          v-for="item in tempProducts"
+          :key="item.id"
+        >
+          <div class="card mb-4">
+            <router-link to="product" :title="item.title">
+              <img :src="item.imageUrl" class="card-img-top" alt="" />
+              <div class="card-body p-2">
+                <div class="d-flex justify-content-between align-items-center">
+                  <p class="fs-5 ps-2 mt-1 mb-0">{{ item.title }}</p>
+                  <a class="pe-2" href=""><i class="bi bi-star"></i></a>
+                </div>
+                <div
+                  class="
+                    d-flex
+                    justify-content-between
+                    align-items-center
+                    pt-3
+                    pb-2
+                  "
+                >
+                  <button
+                    type="button"
+                    class="btn btn-outline-secondary btn-sm ms-2"
+                  >
+                    加入購物車
+                  </button>
+                  <p class="fs-5 text-end pe-2 mb-0">NT.{{ item.price }}</p>
+                </div>
+              </div>
+            </router-link>
+          </div>
+        </div>
+      </div>
     </section>
   </div>
-  <Pagination :pages="pagination" @emit-pages="getProducts"></Pagination>
+  <Pagination :pages="pagination" @emit-pages="getProducts" v-if="!filterMode"></Pagination>
 </template>
 
 <script>
+import { useToast } from 'vue-toastification';
 import Pagination from '../components/Pagination.vue';
 
 export default {
   data() {
     return {
-      products: {},
+      products: [],
+      tempProducts: [],
+      filterMode: false,
+      keyword: '',
       pagination: {},
     };
   },
@@ -120,6 +192,30 @@ export default {
           this.pagination = res.data.pagination;
         }
       });
+    },
+    filterProducts(category) {
+      if (category) {
+        const arr = this.products.filter((value) => value.category === category);
+        this.tempProducts = arr;
+        this.filterMode = true;
+        this.category = category;
+      } else {
+        this.filterMode = false;
+        this.category = '';
+      }
+    },
+    keywordFilter() {
+      const toast = useToast();
+      if (this.keyword.trim() === '') {
+        toast.info('請於搜尋欄位輸入關鍵字');
+      } else {
+        this.filterMode = true;
+        this.category = '';
+        const keywordFilter = this.keyword.toLowerCase();
+        const arr = this.products.filter((value) => value.title.toLowerCase().match(keywordFilter));
+        this.tempProducts = arr;
+        this.keyword = '';
+      }
     },
   },
   created() {
