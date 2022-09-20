68fe218a0a45c2ecee80fe5033a58c10505551ae
William <willam3305@gmail.com>
2022-09-20 18:46:29 +0800

    置入後台訂單列表內容

---
+++ src/components/OrderModal.vue
@@ -0,0 +1,122 @@
+<template>
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+    ref="modal"
+  >
+    <div class="modal-dialog modal-xl modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header bg-dark text-light">
+          <h5 class="modal-title" id="exampleModalLabel">訂單明細</h5>
+          <button
+            type="button"
+            class="btn-close btn-close-white"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <div class="row py-2">
+            <div class="col-5">
+              <span class="h4 fw-bold ms-1">用戶資料</span>
+              <table class="table mt-2 mb-4">
+                <tbody>
+                  <tr>
+                    <th>姓名</th>
+                    <td>{{ order.user.name }}</td>
+                  </tr>
+                  <tr>
+                    <th>Email</th>
+                    <td>{{ order.user.email }}</td>
+                  </tr>
+                  <tr>
+                    <th>電話</th>
+                    <td colspan="2">{{ order.user.tel }}</td>
+                  </tr>
+                  <tr>
+                    <th>地址</th>
+                    <td colspan="2">{{ order.user.address }}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <span class="h4 fw-bold ms-1">訂單備註</span>
+              <p class="h5 ms-1 mt-3">"123"</p>
+            </div>
+            <div class="col-7">
+              <span class="h4 fw-bold ms-1">訂單細節</span>
+              <table class="table mt-2 mb-4">
+                <tbody>
+                  <tr>
+                    <th>訂單編號</th>
+                    <td>{{ order.id }}</td>
+                  </tr>
+                  <tr>
+                    <th>下單時間</th>
+                    <td>{{ $filters.date(order.create_at) }}</td>
+                  </tr>
+                  <tr>
+                    <th>付款時間</th>
+                    <td>
+                      <span v-if="order.is_paid">{{ $filters.date(order.paid_date) }}</span>
+                      <span class="text-danger fw-bold" v-else>未付款</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>付款狀態</th>
+                    <td>
+                      <span class="text-success fw-bold" v-if="order.is_paid">已付款</span>
+                      <span class="text-danger fw-bold" v-else>未付款</span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th>總金額</th>
+                    <td>NT. {{ order.total }}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <span class="h4 fw-bold ms-1">選購商品</span>
+              <table class="table mt-2">
+                <tbody>
+                  <tr v-for="(item, i) in order.products" :key="i">
+                    <th>{{ item.product.title }}</th>
+                    <td>{{ item.qty }} / {{ item.product.unit }}</td>
+                    <td>NT. {{ item.product.price }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+          >
+            取消
+          </button>
+          <button type="button" class="btn btn-primary">確認</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import modalMixin from '../assets/mixins/modalMixin';
+
+export default {
+  data() {
+    return {
+      modal: '',
+    };
+  },
+  props: {
+    order: {},
+  },
+  mixins: [modalMixin],
+};
+</script>
--- src/views/ServerOrder.vue
+++ src/views/ServerOrder.vue
@@ -13,23 +13,34 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>2022/8/15</td>
-            <td>test@email.com</td>
+          <tr v-for="order in orders" :key="order.id">
+            <td>{{ $filters.date(order.create_at) }}</td>
+            <td>{{ order.user.email }}</td>
             <td>
-              <ul class="list-unstyled mb-0">
+              <ul
+                class="list-unstyled mb-0"
+                v-for="item in order.products"
+                :key="item.id"
+              >
                 <li>
-                  <small>そばにいて。 數量：1</small>
+                  <small>{{ item.product.title }} 數量：{{ item.qty }}</small>
                 </li>
               </ul>
             </td>
-            <td>NT.350</td>
+            <td>NT. {{ order.total }}</td>
             <td>
               <div class="form-check form-switch">
-                <input class="form-check-input" type="checkbox" role="switch" />
+                <input
+                  class="form-check-input"
+                  type="checkbox"
+                  role="switch"
+                  v-model="order.is_paid"
+                />
                 <label class="form-check-label">
-                  <span class="text-success fw-bold">已付款</span>
-                  <!-- <span class="text-danger fw-bold">未付款</span> -->
+                  <span class="text-success fw-bold" v-if="order.is_paid"
+                    >已付款</span
+                  >
+                  <span class="text-danger fw-bold" v-else>未付款</span>
                 </label>
               </div>
             </td>
@@ -48,7 +59,14 @@
                   class="dropdown-menu dropdown-menu-dark"
                   aria-labelledby="dropdownMenuButton1"
                 >
-                  <li><a class="dropdown-item" href="#">詳細</a></li>
+                  <li>
+                    <a
+                      class="dropdown-item"
+                      href="#"
+                      @click.prevent="openModal(order)"
+                      >詳細</a
+                    >
+                  </li>
                   <li><a class="dropdown-item" href="#">刪除</a></li>
                 </ul>
               </div>
@@ -58,7 +76,42 @@
       </table>
     </div>
   </div>
+  <OrderModal ref="orderModal" :order="tempOrder"></OrderModal>
 </template>
+
+<script>
+import OrderModal from '../components/OrderModal.vue';
+
+export default {
+  data() {
+    return {
+      orders: [],
+      tempOrder: {},
+    };
+  },
+  components: {
+    OrderModal,
+  },
+  methods: {
+    getOrders() {
+      const api = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/admin/orders?page=1`;
+      this.$http.get(api).then((res) => {
+        if (res.data.success) {
+          this.orders = res.data.orders;
+        }
+      });
+    },
+    openModal(order) {
+      this.tempOrder = { ...order };
+      const orderComponents = this.$refs.orderModal;
+      orderComponents.modalShow();
+    },
+  },
+  created() {
+    this.getOrders();
+  },
+};
+</script>
 
 <style lang="scss">
 @import "../assets/components/_Server.scss";
