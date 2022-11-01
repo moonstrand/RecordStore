435b49af4981fb84a56b1b3bfe60427dfdcdc052
William <willam3305@gmail.com>
2022-11-01 16:30:24 +0800

    新增footer連結

--- src/components/UserFooter.vue
+++ src/components/UserFooter.vue
@@ -2,7 +2,14 @@
   <div class="footer-bg py-4">
     <div class="container text-center text-light">
       <p class="mb-1">Copyright © 2022 moonstrand</p>
-      <p class="mb-0">個人練習作品，非商業用途。</p>
+      <p class="mb-1">個人練習作品，非商業用途。</p>
+      <a class="text-light footer-links" href="https://github.com/moonstrand">
+        <i class="bi bi-github pe-1"></i>GitHub
+      </a>
     </div>
   </div>
 </template>
+
+<style lang="scss">
+@import "@/assets/scss/all.scss";
+</style>
